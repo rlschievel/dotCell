4047cb88c370d3acf3abb8fe911c9e8f421f6d3b
Randall <rlschievel@gmail.com>
2020-10-14 23:53:07 -0500

    Moved the MainWindowView to the Views folder.

--- dotCell/Views/MainWindowView.xaml.cs
+++ dotCell/Views/MainWindowView.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using dotCell.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -13,16 +14,17 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace dotCell
+namespace dotCell.Views
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindowView : Window
     {
-        public MainWindow()
+        public MainWindowView()
         {
             InitializeComponent();
+            DataContext = new MainWindowViewModel();
         }
     }
 }
